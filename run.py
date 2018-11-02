785de33cc666db914f8f0dddb1d255ce7100d9d0
James Portman <jamesp@localhost.localdomain>
2018-11-02 16:39:27 +0000

    boop

--- run.py
+++ run.py
@@ -455,7 +455,7 @@ while True:
             "If the most-significant bit of Vx is 1, then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2."
             if DEBUG:
                 print "8xyE - SHL Vx {, Vy} - Set Vx = Vx SHL 1"
-            register_v[int("F", 16)] = (register_v[second_nibble] & int(b'10000000')) >> 7
+            register_v[int("F", 16)] = register_v[second_nibble] >> 7
             register_v[second_nibble] = register_v[second_nibble] << 1
             continue
 
